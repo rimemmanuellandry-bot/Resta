2b8672e68516061dc378dd8c6e53348279a0af5b
Rim Emmanuel <rimemmanuellandry@gmail.com>
2026-09-08 13:58:52 +0100

    Traduction FR/EN de la pages Bienvenue

--- src/app/pages/accueil/accueil.ts
+++ src/app/pages/accueil/accueil.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-accueil',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
